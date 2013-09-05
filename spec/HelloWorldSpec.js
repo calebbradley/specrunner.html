cb000f5e6f811f7e11682890569bc482d03524b2
Caleb <calebbradley30@yahoo.com>
2013-09-04 23:20:55 -0400

    12SpecsPassed

--- spec/HelloWorldSpec.js
+++ spec/HelloWorldSpec.js
@@ -21,3 +21,77 @@ describe("pig!", function(){
 		expect(pig()).toEqual('Pond Swimmer!');
 	});
 });
+
+describe("man", function(){
+	it("Farmer", function(){
+		expect(man()).toEqual('Farmer');
+	});
+});
+
+describe("tractor", function(){
+	it("Is a noisy machine!", function(){
+		expect(tractor()).toEqual('noisy machine!');
+	});
+});
+
+describe("barn", function(){
+	it("Full of Squeaky Doors!", function(){
+		expect(barn()).toEqual('Squeaky Doors!');
+	});
+});
+
+describe("fence", function(){
+	it("Horse lands on and breaks me!", function(){
+		expect(fence()).toEqual('Horse breaks me!');
+	});
+});
+
+describe("cart", function(){
+	it("Horse pulls me!", function(){
+		expect(cart()).toEqual('Horse pulls me!');
+	});
+});
+
+describe("haybale", function(){
+	it("Horse eats me!", function(){
+		expect(haybale()).toEqual('Horse eats me!');
+	});
+});
+
+describe("sun", function(){
+	it("Makes grass grow!", function(){
+		expect(sun()).toEqual('Makes grass grow!');
+	});
+});
+
+describe("tree", function(){
+	it("Cool place to sleep under!", function(){
+		expect(tree()).toEqual('Cool place to sleep under!');
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/helloWorld.js
+++ src/helloWorld.js
@@ -7,9 +7,50 @@ var Silver = function() {
 };
 
 var horse = function() {
-	return "Fence Jumper!"
+	return "Fence Jumper!";
 };
 
 var pig = function() {
-	return "Pond Swimmer!"
+	return "Pond Swimmer!";
 };
+
+var man = function() {
+	return "Farmer";
+};
+
+var tractor = function() {
+	return "noisy machine!";
+};
+
+var barn = function() {
+	return "Squeaky Doors!";
+};
+
+var fence = function() {
+	return "Horse breaks me!";
+};
+
+var cart = function() {
+	return "Horse pulls me!";
+};
+
+var haybale = function() {
+	return "Horse eats me!";
+};
+
+var sun = function() {
+	return "Makes grass grow!";
+};
+
+var tree = function() {
+	return "Cool place to sleep under!";
+};
+
+
+
+
+
+
+
+
+
